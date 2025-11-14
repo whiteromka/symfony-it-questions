a1c43c5f62b01be73730de43064c93bc987ff04d
Rom <whiteromka@yandex.ru>
2025-11-14 23:10:48 +0300

    Добавил имущество для пользователей на Vue js

--- assets/js/vue-components/User.js
+++ assets/js/vue-components/User.js
@@ -1,12 +1,31 @@
 const User = {
     template: `
-        <div class="user">
-            <span> Имя: {{ user.name }} {{ user.lastName }} {{ user.email }}</span>
-            <button class="btn btn-danger" @click="removeUser()">x</button>
+        <div class="user cp" :class="selectedUserByEmail === user.email ? 'bordered' : ''">
+        <div class="user cp" :class="selectedUserByEmail === user.email ? 'bordered' : ''">
+            <div class="row">
+                <div class="col-sm-8">
+                    <span> Имя: {{ user.name }} {{ user.lastName }} {{ user.email }}</span>
+                </div>
+                <div class="col-sm-4 text-end">
+                    <button class="btn btn-primary" @click="$emit('add-estate', user)">
+                        <i class="fa fa-address-card" aria-hidden="true"></i>
+                    </button>
+                    <button class="btn btn-danger" @click="$emit('remove-user', user.email)">
+                        <i class="fa fa-times-circle" aria-hidden="true"></i>
+                    </button>
+                </div>
+            </div>
+            <div v-if="user.estates && user.estates.length > 0">
+                <ul>
+                    <li v-for="(estate, idx) in user.estates" :key="idx">
+                    {{ estate.estate }} {{ estate.cost }}
+                    </li>
+                </ul>
+            </div>
         </div>
     `,
 
-    props: ['user'],
+    props: ['user', 'selectedUserByEmail'],
     methods: {
         removeUser() {
             this.$emit('remove-user', this.user.email);
--- assets/js/vue-components/UserList.js
+++ assets/js/vue-components/UserList.js
@@ -1,41 +1,78 @@
 const UserList = {
     template: `
         <div class="user-list">
-            <h3>Мой список дел</h3>
-            
-            <!-- Поле для ввода нового дела -->
-            <div class="mb-3">
-                <input 
-                    v-model="userName" 
-                    type="text" 
-                    class="form-control mb-1" 
-                    placeholder="Введите имя"
-                >
-                 <input 
-                    v-model="userLastName" 
-                    type="text" 
-                    class="form-control mb-1" 
-                    placeholder="Введите фамилию"
-                >
-                <input 
-                    v-model="email" 
-                    type="text" 
-                    class="form-control mb-1" 
-                    placeholder="Введите email"
-                >
-                <button @click="addUser()" class="btn btn-primary">
-                    Добавить пользователя
-                </button>
-            </div>
-            <hr>
-            
-            <!-- Список дел -->
-            <div class="todos">
-                <user 
-                    v-for="(user, index) in users" :key="user.id"
-                    :user="user"
-                    @remove-user="removeUser" 
-                ></user>
+            <div class="row">
+                <div class="col-sm-6">
+                    <!--  -->
+                    <div class="users">
+                        <user 
+                            v-for="(user, index) in users" :key="user.id"
+                            :user="user"
+                            :selectedUserByEmail="selectedUserByEmail"
+                            @remove-user="removeUser" 
+                            @add-estate="addEstate(user)"
+                        ></user>
+                    </div>
+                </div>
+                
+                 <div class="col-sm-6">
+                    <div class="btn-group" role="group" aria-label="Простой пример">
+                        <button type="button" class="btn " 
+                            :class="formMode==='user' ? 'btn-primary' : 'btn-outline-secondary'" 
+                            @click="formMode='user'">Пользователи</button>
+                          
+                        <button type="button" class="btn" :disabled="selectedUserByEmail===''"
+                           :class="formMode==='estate' ? 'btn-primary' : 'btn-outline-secondary'" 
+                           @click="formMode='estate'">Имущество</button>
+                    </div>
+                    <div v-if="formMode === 'user'">
+                        <h3>Пользователи</h3>
+                        <div class="mb-3">
+                            <input 
+                                v-model="userName" 
+                                type="text" 
+                                class="form-control mb-1" 
+                                placeholder="Введите имя"
+                            >
+                             <input 
+                                v-model="userLastName" 
+                                type="text" 
+                                class="form-control mb-1" 
+                                placeholder="Введите фамилию"
+                            >
+                            <input 
+                                v-model="email" 
+                                type="text" 
+                                class="form-control mb-1" 
+                                placeholder="Введите email"
+                            >
+                            <button @click="addUser()" class="btn btn-success">
+                                Добавить пользователя
+                            </button>
+                        </div>
+                    </div>
+                    <div v-else>
+                        <h3>Имущество {{ selectedUserByEmail }}</h3>
+                        <div class="mb-3">
+                            <input 
+                                v-model="estate" 
+                                type="text" 
+                                class="form-control mb-1" 
+                                placeholder="Имущество"
+                            >
+                             <input 
+                                v-model="cost" 
+                                type="text" 
+                                class="form-control mb-1" 
+                                placeholder="Стоимость"
+                            >
+                            <button @click="saveEstate()" class="btn btn-success">
+                                Добавить имущество
+                            </button>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
         </div>
     `,
@@ -49,14 +86,32 @@ const UserList = {
             userName: '',
             userLastName: '',
             email: '',
+            selectedUserByEmail: '',
+            formMode: 'user', //user/estate
+            estate: '',
+            cost: 0,
+        }
+    },
+    watch: {
+        users(newUsers, oldUsers) {
+            if(this.users.length === 0 ||  this.selectedUserByEmail == '') {
+                this.formMode = 'user'
+            }
+        },
+        formMode(newMode, oldMode) {
+            if (newMode === 'user') {
+                this.selectedUserByEmail = ''
+            }
         }
     },
     methods: {
+        select(email) {
+            this.selectedUserByEmail = email
+        },
         addUser() {
             if (this.userName.trim() !== "" && this.userLastName.trim() !== "" && this.email.trim() !== "") {
                 let user = this.createNewUser(this.userName, this.userLastName, this.email)
-                this.$emit('add-user', user); // Исправлено: используем $emit
-                // Очищаем поля после добавления
+                this.$emit('add-user', user);
                 this.userName = '';
                 this.userLastName = '';
                 this.email = '';
@@ -66,7 +121,7 @@ const UserList = {
         },
         createNewUser(name, userLastName, email) {
             return {
-                id: Date.now(), // Добавляем ID для ключа
+                id: Date.now(),
                 name: name,
                 lastName: userLastName,
                 email: email,
@@ -77,10 +132,28 @@ const UserList = {
                     "user",
                     "ROLE_USER"
                 ],
+                estates: []
             }
         },
         removeUser(email) {
+            if (email === this.selectedUserByEmail) {
+                this.selectedUserByEmail = ''
+                this.formMode = 'estate'
+            }
             this.$emit('remove-user', email);
+        },
+        addEstate(user) {
+            this.formMode = 'estate'
+            this.selectedUserByEmail = user.email
+        },
+        saveEstate() {
+            this.$emit('add-estate-to-user', {
+                userEmail: this.selectedUserByEmail,
+                estate: {
+                    estate: this.estate,
+                    cost: this.cost
+                }
+            });
         }
     }
 }
