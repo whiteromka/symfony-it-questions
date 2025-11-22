372bfbbddd86332cebe92c64749f3d5caa071087
Rom <whiteromka@yandex.ru>
2025-11-23 02:35:46 +0300

    Добавил подтверждение удаления

--- src/Dto/Input/SkillRequestDto.php
+++ src/Dto/Input/SkillRequestDto.php
@@ -9,7 +9,7 @@ class SkillRequestDto
 {
     #[Assert\NotBlank(message: 'Название не может быть пустым')]
     #[Assert\Length(
-        min: 3,
+        min: 2,
         max: 255,
         minMessage: 'Заголовок должен быть длиннее {{ limit }} символов',
         maxMessage: 'Заголовок не может быть короче {{ limit }} символов'
